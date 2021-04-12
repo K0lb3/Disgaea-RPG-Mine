4d6c7d567783dda4ecc1dd461624571f49b5282a
K0lb3 <rudolf.kolbe@st.ovgu.de>
2021-04-12 15:05:32 +0200

    Update to Unity 1.7

--- download_assets.py
+++ download_assets.py
@@ -3,7 +3,7 @@ import UnityPy
 import re
 import os
 
-from lib.api import base_api, asset_api, ASSETS, RES
+from lib import base_api, asset_api, ASSETS, RES, AssetBatchConverter
 
 
 # init
@@ -13,6 +13,9 @@ ASSET_LIST_FP = os.path.join(ASSETS, "asset_list.txt")
 
 SYSTEM = "android"
 
+def toDict(tuples):
+    return {key:val for key,val in tuples}
+
 def main():
     android = asset_api.get(f"/{SYSTEM}/{SYSTEM}").content
 
@@ -20,18 +23,30 @@ def main():
     asset_list_f = open(ASSET_LIST_FP, "at", encoding="utf8")
 
     try:
+        TODO = []
         for obj in UnityPy.load(android).objects:
             if obj.type == "AssetBundleManifest":
-                d = obj.read()
-                for aid, name in d.AssetBundleNames.items():
-                    ahash = bytes(d.AssetBundleInfos[aid].AssetBundleHash.values()).hex()
+                d = obj.read_typetree()
+                names = toDict(d["AssetBundleNames"])
+                infos = toDict(d["AssetBundleInfos"])
+                for aid, name in names.items():
+                    ahash = bytes(infos[aid]["AssetBundleHash"].values()).hex()
                     
                     if name not in asset_list or asset_list[name] != ahash:
-                        data = download_asset(name, RAW)
-                        #extract_asset(data, get_path(EXT, name))
-                        asset_list_f.write(f"{name}\t{ahash}\n")
-                        print(name)
-    except KeyboardInterrupt(e):
+                        TODO.append((name, ahash))
+        if TODO:
+            for i, (name, ahash) in enumerate(TODO):
+                print(f"{i+1}/{len(TODO)} : {name}")
+                data = download_asset(name, RAW)
+                extract_asset(data, get_path(EXT, name))
+                asset_list_f.write(f"{name}\t{ahash}\n")
+
+                if i%10:
+                    # save current progress
+                    asset_list_f.close()
+                    asset_list_f = open(ASSET_LIST_FP, "at", encoding="utf8")
+            asset_list_f.close()
+    except KeyboardInterrupt as e:
         pass
     asset_list_f.close()
 
@@ -45,53 +60,31 @@ def read_asset_list():
             if line.strip()
         )
 
-def get_path(dir, name):
-    fp = os.path.join(dir, *name.split("/"))
+def get_path(path, name):
+    fp = os.path.join(path, *name.split("/"))
     os.makedirs(os.path.dirname(fp), exist_ok=True)
     return fp
 
-def download_asset(name, dir : str = ""):
+def download_asset(name, dir_path : str = ""):
     # dir : if set, save file into that dir
     data = asset_api.get(f"/{SYSTEM}/{name}").content
-    if dir:
-        with open(get_path(dir, name), "wb") as f:
+    if dir_path:
+        with open(get_path(dir_path, name), "wb") as f:
             f.write(data)
     return data
 
-def extract_asset(inp, dir):
-    # inp can be bytes or a file path
-    # dir - path to save the assets into
+def extract_asset(inp, path):
     env = UnityPy.load(inp)
-    for obj in env.objects:
-        data = obj.read()
-        fp, extension = os.path.splitext(data.name)
-        fp = get_path(dir, fp)
-
-        if obj.type == 'TextAsset':
-            if not extension:
-                extension = '.txt'
-            with open(f"{fp}{extension}", 'wb') as f:
-                f.write(data.script)
-
-        elif obj.type == "Sprite":
-            extension = ".png"
-            data.image.save(f"{fp}{extension}")
-
-            return [obj.path_id, data.m_RD.texture.path_id, getattr(data.m_RD.alphaTexture, 'path_id', None)]
-
-        elif obj.type == "Texture2D":
-            extension = ".png"
-            fp = f"{fp}{extension}"
-            if not os.path.exists(fp):
-                try:
-                    data.image.save(fp)
-                except EOFError:
-                    pass
-        
-        elif obj.type == "AudioClip":
-            for sample in data.samples:
-                print()
-
+    # make sure that Texture2Ds are at the end
+    objs = sorted((obj for obj in env.objects if obj.type.name in AssetBatchConverter.TYPES), key = lambda x: 1 if x.type == "Texture2D" else 0)
+    # check how the path should be handled
+    if len(objs) == 2 and objs[0].type == "Sprite" and objs[1].type == "Texture2D":
+        AssetBatchConverter.export_obj(objs[0], os.path.dirname(path), True)
+    else:
+        used = []
+        for obj in objs:
+            if obj.path_id not in used:
+                used.extend(AssetBatchConverter.export_obj(obj, path, True))
 
 if __name__ == "__main__":
     main()
